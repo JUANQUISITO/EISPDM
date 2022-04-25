1483f1aa582c0c4ac3a295770fe10fe9d61dd234
Franzr2022 <franz.huanaco@uab.edu.bo>
2022-04-25 19:10:46 -0400

    Update actualizar_clave.php

--- actualizar_clave.php
+++ actualizar_clave.php
@@ -1,10 +1,10 @@
 <?php
-$Usuario=$_POST["Usuario"];
-$Clave=$_POST["Clave"];
-$NClave=$_POST["Nueva_Clave"];
+$Usuario=$_POST["usuario"];
+$Clave=$_POST["clave"];
+$NClave=$_POST["nclave"];
 $conexion=mysql_connect("localhost","root","");
 mysql_select_db("bd_seguimiento",$conexion);
-$consult=mysql_query("UPDATE encargado SET Contraseña=$NClave WHERE Usuario=$Usuario and Contraseña=$Clave");
+$consult=mysql_query("UPDATE encargado SET password=$NClave WHERE username=$Usuario and password=$Clave");
 echo "Se cambio la contraseña con éxito <br>";
-echo "<a href=index.hmtl> Volver al principio </a>";
+echo "<a href=index.html> Volver al principio </a>";
 ?>
